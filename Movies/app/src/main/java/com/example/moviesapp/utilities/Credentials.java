4f545c758f11a876f14ec2118b7c9f2e6830c3ac
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-18 13:55:41 +0530

    credentials and dependency
    -- defining BASE_URL and API_KEY
    -- adding restAPI dependencies (retrofit and gson-convertor)

---
+++ Movies/app/src/main/java/com/example/moviesapp/utilities/Credentials.java
@@ -0,0 +1,7 @@
+package com.example.moviesapp.utilities;
+
+public class Credentials {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/";
+    public static final String API_KEY = "c85fa586d8a2371f5e17f63be9ba94c0";
+}
