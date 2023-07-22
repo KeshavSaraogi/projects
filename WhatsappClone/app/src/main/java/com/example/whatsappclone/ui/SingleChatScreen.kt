c3bbf3c687b5d050150d2c6f55982961e4e7ba55
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-07-22 17:00:09 +0530

    screen placeholders
    -- creating screen placeholders for all the screens that would be eventually required with a text message

---
+++ WhatsappClone/app/src/main/java/com/example/whatsappclone/ui/SingleChatScreen.kt
@@ -0,0 +1,17 @@
+package com.example.whatsappclone.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun SingleChatScreen(chatID: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "SINGLE CHAT SCREEN $chatID")
+    }
+}
