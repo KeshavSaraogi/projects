f6c7c69068dbdc2d30a83bee1feed8caa3217dc6
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-08-07 23:24:12 +0530

    retrofit
    -- making an api function to get a sample result
    -- adding an empty file for the remote data source
    -- changing project structure

---
+++ Recipe/app/src/main/java/com/example/recipe/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.recipe
+
+class Constants  {
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "faf71764796b44f6a4a107a26911e3f4"
+    }
+}
---
+++ Recipe/app/src/main/java/com/example/recipe/FoodRecipeAPI.kt
@@ -0,0 +1,14 @@
+package com.example.recipe
+
+import com.example.recipe.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipeAPI {
+
+    @GET("/recipes/complexSearch")
+    fun getRecipes(
+        @QueryMap queries: Map<String,String>
+    ) : Response<FoodRecipe>
+}
