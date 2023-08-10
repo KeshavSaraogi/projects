38c36ef6bf97e43d432e211226bddff8e85e9e18
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-08-10 19:06:19 +0530

    incomplete

--- Recipe/app/src/main/java/com/example/recipe/util/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.recipe.util
-
-class Constants  {
-    companion object {
-
-        const val BASE_URL = "https://api.spoonacular.com"
-        const val API_KEY = "faf71764796b44f6a4a107a26911e3f4"
-    }
-}
